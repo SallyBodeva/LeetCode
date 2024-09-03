1bb845ff035e80d59f5046e5a5aa296d4d2d4b2a
salihe <salihe.bodeva10@gmail.com>
2024-09-03 22:28:35 +0300

    Daily problems

---
+++ S01-LeetCode/P03-ReplaceTheChalk/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[] chalk = new int[] { 5,1,5};
+        int k = 22;
+        Console.WriteLine(ChalkReplacer(chalk, k));
+    }
+    public static int ChalkReplacer(int[] chalk, int k)
+    {
+        int index = 0;
+        while (k > 0)
+        {
+            for (int i = 0; i < chalk.Length; i++)
+            {
+                k -= chalk[i];
+                if (k < 0)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+        }
+        return index;
+    }
+}
