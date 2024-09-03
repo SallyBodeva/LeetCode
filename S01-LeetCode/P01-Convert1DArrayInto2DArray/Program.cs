6ad7aea4135539f251384a87d8d99a115fad9754
salihe <salihe.bodeva10@gmail.com>
2024-09-03 22:00:52 +0300

    Daily problems

---
+++ S01-LeetCode/P01-Convert1DArrayInto2DArray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //
+    }
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        int[][] result = new int[n][];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                result[i][j] = original[i];
+            }
+        }
+        return result;
+    }
+}
---
+++ S01-LeetCode/P02-SumOfDigitsOfStringAfterConvert/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(GetLucky("leetcode", 2));
+    }
+    public static int GetLucky(string s, int k)
+    {
+        s = new string(s.ToLower());
+        string result = string.Empty;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            result += (int)s[i] - 96;
+        }
+
+        int sum = 0;
+        int total = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            foreach (var item in result)
+            {
+                sum += int.Parse(item.ToString());
+            }
+            result = sum.ToString();
+            if (i != k)
+            {
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+}
+
