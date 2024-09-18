4d03faf8f2660bf169babda8b733316ac4faa90e
salihe <salihe.bodeva10@gmail.com>
2024-09-19 01:06:41 +0300

    Daily problems

---
+++ S01-LeetCode/P10-UncommonWordsFromTwoSentences/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        string s1 = "this apple is sweet";
+        string s2 = "this apple is sour";
+
+        Console.WriteLine(string.Join(" ",UncommonFromSentences(s1,s2)));
+    }
+    public static string[] UncommonFromSentences(string s1, string s2)
+    {
+        string[] firstSentence = s1.Split(" ");
+        string[] secondSentence = s2.Split(" ");
+
+        List<string> result = firstSentence.Where(x => !secondSentence.Contains(x) && firstSentence.Count(word => word == x) == 1).ToList();
+        result.AddRange(secondSentence.Where(x => !firstSentence.Contains(x) && secondSentence.Count(word => word == x) == 1).ToList());
+
+        return result.ToArray();
+    }
+}
---
+++ S01-LeetCode/P11-LargestNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 3, 30, 34, 5, 9 };
+        Console.WriteLine(LargestNumber(nums));
+    }
+    public static string LargestNumber(int[] nums)
+    {
+        string[] elements = nums.Select(x => x.ToString()).ToArray();
+        Array.Sort(elements,PersonalComparison);
+
+        StringBuilder result = new StringBuilder();
+        foreach (var num in elements)
+        {
+            result.Append(num);
+        }
+        return result.ToString();
+    }
+    private static int PersonalComparison(string a, string b)
+    {
+        return (b+a).CompareTo(a + b);
+    }
+}
+
